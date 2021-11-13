50e037bb331ea9d655aed3e1e83a345bb5f07cc5
Dmitry <dmsobolev@yahoo.com>
2021-11-13 09:20:55 +0300

    #3 - basic three setup and readme update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,9 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '../src/Link';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "../src/Link";
+import CubeScene from "../src/3D/cube";
 
 export default function Index() {
   return (
@@ -11,6 +12,9 @@ export default function Index() {
         <Typography variant="h4" component="h1" gutterBottom>
           Hello, Beechain!
         </Typography>
+        <Container sx={{ height: 500 }}>
+          <CubeScene />
+        </Container>
         <Link href="/about" color="secondary">
           Routing test
         </Link>
